b4ee187e115c2348cc2277d0f50b91e2ebb79292
Joseph Simone <averagejoey2000@gmail.com>
2014-12-22 23:01:13 -0800

    Did so freaking much with the file. Read todo and notes for reference."

--- c/ex1.c
+++ c/ex1.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-int main(int argc, char *argv[])
+main(int argc, char *argv[])
 {
-  puts("Hello world.");
-
+  puts("Hello world.  This is the test program. it has strange indentation. \
+Do you like bananas? \
+Well, You're gonna go bananas. \
+ON THE MOOOOOOOOOOOOOOOOOOOOOOOONNNNNNN BEEYATCH!!");
+  puts("I think, but am not sure about of this moment, that compiled C programs understand environment variables. For example $HOME or ~ or otherwise. Nevertheless, i shall know soon enough. about like a fill second before you read this. KTHNXBAI");
   return 0;
 }
