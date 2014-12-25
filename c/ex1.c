98a84a18df2c9343cc2c0980fdfef5305ac26957
Joseph Simone <averagejoey2000@gmail.com>
2014-12-24 17:33:42 -0800

    Spelling fix

--- c/ex1.c
+++ c/ex1.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
-  puts("Hello world.  This is the test program. it has strange indentation. \
-Do you like bananas? \
-Well, You're gonna go bananas. \
-ON THE MOOOOOOOOOOOOOOOOOOOOOOOONNNNNNN BEEYATCH!!");
-  puts("I think, but am not sure about of this moment, that compiled C programs understand environment variables. For example $HOME or ~ or otherwise. Nevertheless, i shall know soon enough. about like a fill second before you read this. KTHNXBAI");
+  puts("Hello world.\n  This is the test program.\n it has strange indentation.\n Do you like bananas?\n Well, You're gonna go bananas.\n ON THE MOOOOOOOOOOOOOOOOOOOOOOOONNNNNNN BEEYATCH!!");
+  puts("I think, but am not sure about of this moment, that compiled C programs understand environment variables. For example $HOME or ~ or otherwise. Nevertheless, I shall know soon enough. about like a full second before you read this. KTHNXBAI");
   return 0;
 }
