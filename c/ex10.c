b74d97b9f5321ad74166761f3cf362320ec72b28
Joseph Simone <averagejoey2000@gmail.com>
2014-12-27 12:59:24 -0800

    Added all changes, YOLO

--- c/ex10.c
+++ c/ex10.c
@@ -2,24 +2,25 @@
 
 int main(int argc, char *argv[])
 {
-	int i = 0;
+    int i = 0;
 
-	/* go though each string in argv
-	why am I skipping argv[0]?*/
-	for(i = 1 < argc; i++) {
-		printf("arg %d: %s\n", i, argv[i]);
-	}
+    // go through each string in argv
+    // why am I skipping argv[0]?
+    for(i = 1; i < argc; i++) {
+        printf("arg %d: %s\n", i, argv[i]);
+    }
 
-	// let's make our own array of strings
-	char *states[] = {
-	"California", "Oregon",
-	"Washington", "Texas"
-	};
-	int num_states = 4;
+    // let's make our own array of strings
+    char *states[] = {
+        "California", "Oregon",
+        "Washington", "Texas"
+    };
+    int num_states = 4;
 
-	for(i = 0; i < num_states; i++) {
-		printf("state %d: %s\n", i, states[i]);
-	}
+    for(i = 0; i < num_states; i++) {
+        printf("state %d: %s\n", i, states[i]);
+    }
 
-	return 0;
+    return 0;
 }
+
