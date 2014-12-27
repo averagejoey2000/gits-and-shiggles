cb404640d4044e95c230724a3b841bd8fc41fd05
Joseph Simone <averagejoey2000@gmail.com>
2014-12-27 09:58:40 -0800

    Here have more code

--- c/ex8.c
+++ c/ex8.c
@@ -3,11 +3,11 @@
 int main(int argc, char *argv[]) 
 {
   int areas[] = {10, 12, 13, 14, 20};
-  char name[] = "Joe";
+  char name[] = "Zed";
   char full_name[] = {
-    'J', 'o', 'e',
-    ' ', 'M', '.',
-    'S', 'i', 'm', 'o', 'n', 'e', '\0'
+    'Z', 'e', 'd',
+    ' ', 'A', '.',
+    'S', 'h', 'a', 'w', '\0'
   };
 
   /* WARNING: On some systems, you may have to change the %ls in this code to a %u sice it will use unsigned ints*/
