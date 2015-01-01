41a23de0198e4c29abd38dc8ef361af2b351fbc4
Joseph Simone <averagejoey2000@gmail.com>
2015-01-01 14:29:37 -0800

    Souped up compile script

--- bash/compile/compile.sh
+++ bash/compile/compile.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+proc=`nproc`
+job=`expr $proc + 2`
 python setup.py install
 python *.py
 ./autogen.sh || autoreconf -i ; ./configure
-make -j`nproc` && make install || sudo make install || su -c 'make install'
+make -j$job && make install || sudo make install || su -c 'make install'
 echo 'done'
